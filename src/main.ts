bf8a1b597b4eb577504ae053c5c13a6310abdae8
Isak <16906089+essarn@users.noreply.github.com>
2022-05-01 05:57:41 +0200

    fix prisma

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { createServer } from "@graphql-yoga/common";
 import SchemaBuilder from "@pothos/core";
+import PrismaPlugin from "@pothos/plugin-prisma";
 import { PrismaClient } from "@prisma/client";
+import type PrismaTypes from "../prisma/pothos-types";
 
 const prisma = new PrismaClient();
 
-// const builder = new SchemaBuilder<{ PrismaTypes: PrismaTypes }>({
-//   plugins: [PrismaPlugin],
-//   prisma: {
-//     client: prisma,
-//   },
-// });
-
-const builder = new SchemaBuilder({});
+const builder = new SchemaBuilder<{ PrismaTypes: PrismaTypes }>({
+  plugins: [PrismaPlugin],
+  prisma: {
+    client: prisma,
+  },
+});
 
 builder.queryType({
   fields: (t) => ({
